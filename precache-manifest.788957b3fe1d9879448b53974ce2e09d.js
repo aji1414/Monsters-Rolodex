d2076fba10bb5b0e27308661326a7bde19187700
aji1414 <ajisofteng@gmail.com>
2020-08-31 16:07:29 +0100

    Updates

---
+++ precache-manifest.788957b3fe1d9879448b53974ce2e09d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a134de578427ffdc3eb05ba05e13f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06fd3b1d3d9e8bba5e0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06fd3b1d3d9e8bba5e0",
+    "url": "/static/js/main.d6cb8b56.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
